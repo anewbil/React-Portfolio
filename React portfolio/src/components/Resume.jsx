ee35b1da8fa3bc616209a91c76b530f1a0da111f
anewbil <2103alexnewbill@gmail.com>
2024-10-09 16:54:25 -0400

    added link for 'resume'

--- React portfolio/src/components/Resume.jsx
+++ React portfolio/src/components/Resume.jsx
@@ -1,6 +1,9 @@
 function Resume(){
     return(
-        <h1 id="Resume">Resume here</h1>
+        <>
+        <h1 id="Resume"></h1>
+        <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">Click for Resume</a>
+        </>
     )
 
 }
